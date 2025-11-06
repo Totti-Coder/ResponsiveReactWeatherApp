a0aa7d11345d26f8f027c4bb32874d74c96bb9ec
Pablo <pablogroza2000@gmail.com>
2025-11-06 19:48:46 +0100

    Agrego funcionalidad a partir de la API call que nos de la humedad, el viento, temperatura... utilizando los estados de REACT

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -1,53 +1,79 @@
-import sunny from "../assets/images/sunny.png"
-import cloudy from "../assets/images/cloudy.png"
-import rainy from "../assets/images/rainy.png"
-import snowy from "../assets/images/snowy.png"
-import {useState} from "react"
-
+import sunny from "../assets/images/sunny.png";
+import cloudy from "../assets/images/cloudy.png";
+import rainy from "../assets/images/rainy.png";
+import snowy from "../assets/images/snowy.png";
+import { useState } from "react";
 
 const WeatherApp = () => {
-    const [data, setData] = useState({})
-    const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
-    const search = async () => {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=Madrid&appid=${apiKey}&units=metric`
-        const res = await fetch(url)
-        const searchData = await res.json()
-        console.log(searchData)
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+
+  const handleInputChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const search = async () => {
+    if (location.trim() !== "") {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric&lang=es`;
+      const res = await fetch(url);
+      const searchData = await res.json();
+      console.log(searchData);
+      setData(searchData);
+      setLocation("");
     }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
   return (
     <div className="container">
-        <div className="weather-app">
-            <div className="search">
-                <div className="search-top">
-                    <i className="fa-solid fa-location-dot"></i>
-                    <div className="location">Madrid</div>
-                </div>
-                <div className="search-bar">
-                    <input type="text" placeholder="Busca una ubicación"/>
-                    <i className="fa-solid fa-magnifying-glass" onClick={search}></i>
-                </div>
-            </div>
-            <div className="weather">
-                <img src={sunny} alt="sunny"/>
-                <div className="weather-type">Despejado</div>
-                <div className="temp">28°</div>
-            </div>
-            <div className="weather-date">
-                <p>Jue, 6 Nov</p>
-            </div>
-            <div className="weather-data">
-                <div className="humidity">
-                    <div className="data-name">Humedad</div>
-                    <i className="fa-solid fa-droplet"></i>
-                    <div className="data">30%</div>
-                </div>
-                 <div className="wind">
-                    <div className="data-name">Viento</div>
-                    <i className="fa-solid fa-wind"></i>
-                    <div className="data">3 km h</div>
-                </div>
-            </div>
+      <div className="weather-app">
+        <div className="search">
+          <div className="search-top">
+            <i className="fa-solid fa-location-dot"></i>
+            <div className="location">{data.name}</div>
+          </div>
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Busca una ubicación"
+              value={location}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+            />
+            <i className="fa-solid fa-magnifying-glass" onClick={search}></i>
+          </div>
         </div>
+        <div className="weather">
+          <img src={sunny} alt="sunny" />
+          <div className="weather-type">
+            {data.weather ? data.weather[0].main : null}
+          </div>
+          <div className="temp">
+            {data.main ? `${Math.floor(data.main.temp)}°` : null}
+          </div>
+        </div>
+        <div className="weather-date">
+          <p>Jue, 6 Nov</p>
+        </div>
+        <div className="weather-data">
+          <div className="humidity">
+            <div className="data-name">Humedad</div>
+            <i className="fa-solid fa-droplet"></i>
+            <div className="data">{data.main ? data.main.humidity : null}%</div>
+          </div>
+          <div className="wind">
+            gy
+            <div className="data-name">Viento</div>
+            <i className="fa-solid fa-wind"></i>
+            <div className="data">{data.wind ? data.wind.speed : null}km/h</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
